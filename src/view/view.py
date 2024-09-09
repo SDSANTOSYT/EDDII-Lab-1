1ca1dfc609bcfadcfb29655c9ec14eae809a0415
Digaes <digaes05@gmail.com>
2024-09-09 04:23:51 -0500

    update_visual

--- src/view/view.py
+++ src/view/view.py
@@ -40,92 +40,92 @@ class OptionWindow(ctk.CTkToplevel):
 
         self.geometry("1400x1000")
         self.title("Operaciones")
-        self.titleLabel = ctk.CTkLabel(self, text="Operaciones", font=("Helvetica", 50, "bold")).place(y=30,relx=0.5, anchor="center")
+        self.titleLabel = ctk.CTkLabel(self, text="Operaciones", font=("Times", 50, "bold")).place(y=40,relx=0.5, anchor="center")
 
         #lado superior izquierdo del cuadro
-        self.leftUpFrame=ctk.CTkFrame(self,width=485,height=440,fg_color="#d4d3da")
+        self.leftUpFrame=ctk.CTkFrame(self,width=485,height=440,fg_color="#fbf2e1")
         self.leftUpFrame.place(y=80,x=10)
         #Widgets del frame Superior Izquierdo
         #insertar nodos
         self.insertEntry=ctk.CTkEntry(self.leftUpFrame, placeholder_text="Insertar pelicula", width=300,height=30)
         self.insertEntry.place(y=10,x=10)
-        self.result_frameI = ctk.CTkFrame(self.leftUpFrame, width=485, height=390, fg_color="#d4d3da")
+        self.result_frameI = ctk.CTkFrame(self.leftUpFrame, width=485, height=390, fg_color="#fbf2e1")
         self.result_frameI.place(y=50, x=10)
         self.insertEntry.bind("<KeyRelease>", lambda event: self.update_list(self.insertEntry, self.result_frameI, search_movie(self.insertEntry.get()), self.result_labels))
         self.result_labels = []  # Lista para guardar los labels que muestran los resultados
-        self.btnEntry=ctk.CTkButton(self.leftUpFrame, text="Insertar", corner_radius=40, font=("Helvetica", 20, "bold"), command=lambda:self.search_Ui(self.insertEntry.get()))
+        self.btnEntry=ctk.CTkButton(self.leftUpFrame, text="Insertar", corner_radius=40,fg_color="white", text_color="#9f4154",border_color="#9f4154",font=("Times", 20, "bold"), command=lambda:self.search_Ui(self.insertEntry.get()))
         self.btnEntry.place(y=10,x=320)
-        self.warning=ctk.CTkLabel(self.leftUpFrame, text="", font=("Helvetica", 12, "bold"))
+        self.warning=ctk.CTkLabel(self.leftUpFrame, text="", font=("Times", 12, "bold"))
         self.warning.place(y=40,x=320)
 
         #lado inferior izquierdo
-        self.leftDownFrame=ctk.CTkFrame(self,width=485,height=450,fg_color="#d4d3da")
+        self.leftDownFrame=ctk.CTkFrame(self,width=485,height=450,fg_color="#fbf2e1")
         self.leftDownFrame.place(y=530,x=10)
         #Widgets del frame Inferior Izquierdo
         #Eliminar
         self.DeleteEntry=ctk.CTkEntry(self.leftDownFrame, placeholder_text="Eliminar pelicula", width=300,height=30)
         self.DeleteEntry.place(y=10,x=10)
-        self.result_frameD = ctk.CTkFrame(self.leftDownFrame, width=485, height=390, fg_color="#d4d3da")
+        self.result_frameD = ctk.CTkFrame(self.leftDownFrame, width=485, height=390, fg_color="#fbf2e1")
         self.result_frameD.place(y=50, x=10)
 
         self.DeleteEntry.bind("<KeyRelease>", lambda event: self.update_list(self.DeleteEntry, self.result_frameD, search_in_tree(), self.result_labels))
-        self.btnDelete=ctk.CTkButton(self.leftDownFrame, text="Eliminar", corner_radius=40, font=("Helvetica", 20, "bold"),command=lambda: self.delete_Ui(self.DeleteEntry.get()))
+        self.btnDelete=ctk.CTkButton(self.leftDownFrame, text="Eliminar", corner_radius=40,fg_color="white", text_color="#9f4154",border_color="#9f4154",font=("Times", 20, "bold"),command=lambda: self.delete_Ui(self.DeleteEntry.get()))
         self.btnDelete.place(y=10,x=320)
         
 
         #lado derecho del cuadrado
-        self.rightFrame=ctk.CTkFrame(self, width=485,height=900,fg_color="#d4d3da")
+        self.rightFrame=ctk.CTkFrame(self, width=485,height=900,fg_color="#fbf2e1")
         self.rightFrame.place(y=80,x=505)
         #Widgets del frame derecho
         #Buscar
         self.searchEntry=ctk.CTkEntry(self.rightFrame,placeholder_text="Buscar Pelicula", width=300,height=30)
         self.searchEntry.place(y=10,x=10)
 
-        self.result_frameE = ctk.CTkFrame(self.rightFrame, width=485, height=380, fg_color="#6d5eb2")
+        self.result_frameE = ctk.CTkFrame(self.rightFrame, width=485, height=380, fg_color="#fbf2e1")
         self.result_frameE.place(y=220, x=10)
 
         self.searchEntry.bind("<KeyRelease>", lambda event: self.update_list2(self.searchEntry, self.result_frameE, search_filter(bool(self.yearCheck.get()),self.yearEntry.get(),bool(self.percentCheck.get()),bool(self.intIncomeCheck.get()),self.intIncome.get(),self.searchEntry.get()), self.result_labels))
-        self.btnSearch=ctk.CTkButton(self.rightFrame, text="Buscar", corner_radius=40, font=("Helvetica", 20, "bold"))
+        self.btnSearch=ctk.CTkButton(self.rightFrame, text="Buscar",fg_color="white", text_color="#9f4154",border_color="#9f4154",corner_radius=40, font=("Times", 20, "bold"))
         self.btnSearch.place(y=10,x=320)
         #Filtros
         self.var_percentCheck=ctk.BooleanVar()
-        self.percentCheck=ctk.CTkCheckBox(self.rightFrame, text="Ingresos nacional menor a ingresos internacional", variable=self.var_percentCheck, width=50, height=50, command= lambda: self.update_list2(self.searchEntry, self.result_frameE, search_filter(bool(self.yearCheck.get()),self.yearEntry.get(),bool(self.percentCheck.get()),bool(self.intIncomeCheck.get()),self.intIncome.get(),self.searchEntry.get()), self.result_labels))
+        self.percentCheck=ctk.CTkCheckBox(self.rightFrame, text="Ingresos nacional menor a ingresos internacional",font=("Times", 15,),variable=self.var_percentCheck, width=50, height=50, command= lambda: self.update_list2(self.searchEntry, self.result_frameE, search_filter(bool(self.yearCheck.get()),self.yearEntry.get(),bool(self.percentCheck.get()),bool(self.intIncomeCheck.get()),self.intIncome.get(),self.searchEntry.get()), self.result_labels))
         self.percentCheck.place(y=50,x=10)
         self.var_intIncome=ctk.BooleanVar()
-        self.intIncomeCheck = ctk.CTkCheckBox(self. rightFrame, text="Ingresos internacionales mayores a", variable=self.var_intIncome,width=50,height=50, command= lambda: self.update_list2(self.searchEntry, self.result_frameE, search_filter(bool(self.yearCheck.get()),self.yearEntry.get(),bool(self.percentCheck.get()),bool(self.intIncomeCheck.get()),self.intIncome.get(),self.searchEntry.get()), self.result_labels))
+        self.intIncomeCheck = ctk.CTkCheckBox(self. rightFrame, text="Ingresos internacionales mayores a",font=("Times", 17,), variable=self.var_intIncome,width=50,height=50, command= lambda: self.update_list2(self.searchEntry, self.result_frameE, search_filter(bool(self.yearCheck.get()),self.yearEntry.get(),bool(self.percentCheck.get()),bool(self.intIncomeCheck.get()),self.intIncome.get(),self.searchEntry.get()), self.result_labels))
         self.intIncomeCheck.place(y=100,x=10)
-        self.intIncome= ctk.CTkEntry(self.rightFrame,placeholder_text=" Ingresos internacionales",width=190, height=50)
+        self.intIncome= ctk.CTkEntry(self.rightFrame,placeholder_text=" Ingresos internacionales",width=190, height=50, font=("Times", 17,))
         self.intIncome.place(y=100, x=270)
 
         self.var_yearCheck=ctk.BooleanVar()
-        self.yearCheck=ctk.CTkCheckBox(self.rightFrame, text="Año", variable=self.var_yearCheck, width=50, height=50, command= lambda: self.update_list2(self.searchEntry, self.result_frameE, search_filter(bool(self.yearCheck.get()),self.yearEntry.get(),bool(self.percentCheck.get()),bool(self.intIncomeCheck.get()),self.intIncome.get(),self.searchEntry.get()), self.result_labels))
+        self.yearCheck=ctk.CTkCheckBox(self.rightFrame, text="Año", variable=self.var_yearCheck, width=50, height=50, font=("Times", 17,), command= lambda: self.update_list2(self.searchEntry, self.result_frameE, search_filter(bool(self.yearCheck.get()),self.yearEntry.get(),bool(self.percentCheck.get()),bool(self.intIncomeCheck.get()),self.intIncome.get(),self.searchEntry.get()), self.result_labels))
         self.yearCheck.place(y=160,x=10)
 
 
-        self.yearEntry=ctk.CTkEntry(self.rightFrame,placeholder_text="año de estreno", width=120,height=50)
+        self.yearEntry=ctk.CTkEntry(self.rightFrame,placeholder_text="año de estreno", width=120,height=50, font=("Times", 17,))
         self.yearEntry.place(y=160,x=80)
 
 
         #Cuadrado inferior derecho para dar
-        self.frameDownRigth=ctk.CTkFrame(self.rightFrame,width=485, height=180, fg_color="#6d5eb2")
+        self.frameDownRigth=ctk.CTkFrame(self.rightFrame,width=485, height=180, fg_color="#f7e5c4")
         self.frameDownRigth.place(y=720,x=0)
         #Datos del cuadrado
-        self.nodeLevelLbl=ctk.CTkLabel(self.frameDownRigth, text="Nivel del nodo", font=("Helvetica", 20, "bold"))
+        self.nodeLevelLbl=ctk.CTkLabel(self.frameDownRigth, text="Nivel del nodo", font=("Times", 20, "bold"))
         self.nodeLevelLbl.place(y=2)
-        self.balanceLbl=ctk.CTkLabel(self.frameDownRigth, text="Factor de balanceo", font=("Helvetica", 20, "bold"))
+        self.balanceLbl=ctk.CTkLabel(self.frameDownRigth, text="Factor de balanceo", font=("Times", 20, "bold"))
         self.balanceLbl.place(y=32)
-        self.fatherLbl=ctk.CTkLabel(self.frameDownRigth, text="Padre del nodo", font=("Helvetica", 20, "bold"))
+        self.fatherLbl=ctk.CTkLabel(self.frameDownRigth, text="Padre del nodo", font=("Times", 20, "bold"))
         self.fatherLbl.place(y=62)
-        self.gfatherLbl=ctk.CTkLabel(self.frameDownRigth, text="Abuelo del nodo", font=("Helvetica", 20, "bold"))
+        self.gfatherLbl=ctk.CTkLabel(self.frameDownRigth, text="Abuelo del nodo", font=("Times", 20, "bold"))
         self.gfatherLbl.place(y=92)
-        self.uncleLbl=ctk.CTkLabel(self.frameDownRigth, text="Tio del nodo", font=("Helvetica", 20, "bold"))
+        self.uncleLbl=ctk.CTkLabel(self.frameDownRigth, text="Tio del nodo", font=("Times", 20, "bold"))
         self.uncleLbl.place(y=122)
 
         #cuadro de verdad a la derecha
 
-        self.rigth=ctk.CTkFrame(self, width=390,height=900,fg_color="#d4d3da")
+        self.rigth=ctk.CTkFrame(self, width=390,height=900,fg_color="#fbf2e1")
         self.rigth.place(y=80,x=1000)
-        self.title2Label = ctk.CTkLabel(self.rigth, text="Datos", font=("Helvetica", 50, "bold")).place(y=30,relx=0.5, anchor="center")
+        self.title2Label = ctk.CTkLabel(self.rigth, text="Datos", font=("Times", 50, "bold")).place(y=30,relx=0.5, anchor="center")
 
 
 
