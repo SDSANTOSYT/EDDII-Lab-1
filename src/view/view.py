e2c72b9f3b014f50e77ceeaddf063ad60f2cd289
ALEXYSALCED0 <alexysalcedo01@gmail.com>
2024-09-09 09:47:40 -0500

    añadiendo cuadros para presentar datos

--- src/view/view.py
+++ src/view/view.py
@@ -73,59 +73,94 @@ class OptionWindow(ctk.CTkToplevel):
         self.btnDelete.place(y=10,x=320)
         
 
-        #lado derecho del cuadrado
-        self.rightFrame=ctk.CTkFrame(self, width=485,height=900,fg_color="#fbf2e1")
-        self.rightFrame.place(y=80,x=505)
+        #lado central del cuadrado
+        self.centerFrame=ctk.CTkFrame(self, width=485,height=900,fg_color="#fbf2e1")
+        self.centerFrame.place(y=80,x=505)
         #Widgets del frame derecho
         #Buscar
-        self.searchEntry=ctk.CTkEntry(self.rightFrame,placeholder_text="Buscar Pelicula",font=("Times", 15,), width=300,height=30)
+        self.searchEntry=ctk.CTkEntry(self.centerFrame,placeholder_text="Buscar Pelicula",font=("Times", 15,), width=300,height=30)
         self.searchEntry.place(y=10,x=10)
 
-        self.result_frameE = ctk.CTkFrame(self.rightFrame, width=485, height=380, fg_color="#fbf2e1")
+        self.result_frameE = ctk.CTkFrame(self.centerFrame, width=485, height=380, fg_color="#fbf2e1")
         self.result_frameE.place(y=220, x=10)
 
         self.searchEntry.bind("<KeyRelease>", lambda event: self.update_list2(self.searchEntry, self.result_frameE, search_filter(bool(self.yearCheck.get()),self.yearEntry.get(),bool(self.percentCheck.get()),bool(self.intIncomeCheck.get()),self.intIncome.get(),self.searchEntry.get()), self.result_labels))
-        self.btnSearch=ctk.CTkButton(self.rightFrame, text="Buscar",fg_color="white",hover_color="#e9e9e9", text_color="#9f4154",border_color="#9f4154",corner_radius=40, font=("Times", 20, "bold"))
+        self.btnSearch=ctk.CTkButton(self.centerFrame, text="Buscar",fg_color="white",hover_color="#e9e9e9", text_color="#9f4154",border_color="#9f4154",corner_radius=40, font=("Times", 20, "bold"))
         self.btnSearch.place(y=10,x=320)
         #Filtros
         self.var_percentCheck=ctk.BooleanVar()
-        self.percentCheck=ctk.CTkCheckBox(self.rightFrame, text="Ingresos nacional menor a ingresos internacional",font=("Times", 15,),variable=self.var_percentCheck, hover_color= "#9f4154" ,width=50, height=50, command= lambda: self.update_list2(self.searchEntry, self.result_frameE, search_filter(bool(self.yearCheck.get()),self.yearEntry.get(),bool(self.percentCheck.get()),bool(self.intIncomeCheck.get()),self.intIncome.get(),self.searchEntry.get()), self.result_labels))
+        self.percentCheck=ctk.CTkCheckBox(self.centerFrame, text="Ingresos nacional menor a ingresos internacional",font=("Times", 15,),variable=self.var_percentCheck, hover_color= "#9f4154" ,width=50, height=50, command= lambda: self.update_list2(self.searchEntry, self.result_frameE, search_filter(bool(self.yearCheck.get()),self.yearEntry.get(),bool(self.percentCheck.get()),bool(self.intIncomeCheck.get()),self.intIncome.get(),self.searchEntry.get()), self.result_labels))
         self.percentCheck.place(y=50,x=10)
         self.var_intIncome=ctk.BooleanVar()
-        self.intIncomeCheck = ctk.CTkCheckBox(self. rightFrame, text="Ingresos internacionales mayores a",font=("Times", 17,), variable=self.var_intIncome,width=50,hover_color="#9f4154",height=50, command= lambda: self.update_list2(self.searchEntry, self.result_frameE, search_filter(bool(self.yearCheck.get()),self.yearEntry.get(),bool(self.percentCheck.get()),bool(self.intIncomeCheck.get()),self.intIncome.get(),self.searchEntry.get()), self.result_labels))
+        self.intIncomeCheck = ctk.CTkCheckBox(self. centerFrame, text="Ingresos internacionales mayores a",font=("Times", 17,), variable=self.var_intIncome,width=50,hover_color="#9f4154",height=50, command= lambda: self.update_list2(self.searchEntry, self.result_frameE, search_filter(bool(self.yearCheck.get()),self.yearEntry.get(),bool(self.percentCheck.get()),bool(self.intIncomeCheck.get()),self.intIncome.get(),self.searchEntry.get()), self.result_labels))
         self.intIncomeCheck.place(y=100,x=10)
-        self.intIncome= ctk.CTkEntry(self.rightFrame,placeholder_text=" Ingresos internacionales",width=190, height=50, font=("Times", 17,))
+        self.intIncome= ctk.CTkEntry(self.centerFrame,placeholder_text=" Ingresos internacionales",width=190, height=50, font=("Times", 17,))
         self.intIncome.place(y=100, x=270)
 
         self.var_yearCheck=ctk.BooleanVar()
-        self.yearCheck=ctk.CTkCheckBox(self.rightFrame, text="Año", variable=self.var_yearCheck, width=50, height=50, hover_color= "#9f4154",font=("Times", 17,), command= lambda: self.update_list2(self.searchEntry, self.result_frameE, search_filter(bool(self.yearCheck.get()),self.yearEntry.get(),bool(self.percentCheck.get()),bool(self.intIncomeCheck.get()),self.intIncome.get(),self.searchEntry.get()), self.result_labels))
+        self.yearCheck=ctk.CTkCheckBox(self.centerFrame, text="Año", variable=self.var_yearCheck, width=50, height=50, hover_color= "#9f4154",font=("Times", 17,), command= lambda: self.update_list2(self.searchEntry, self.result_frameE, search_filter(bool(self.yearCheck.get()),self.yearEntry.get(),bool(self.percentCheck.get()),bool(self.intIncomeCheck.get()),self.intIncome.get(),self.searchEntry.get()), self.result_labels))
         self.yearCheck.place(y=160,x=10)
 
 
-        self.yearEntry=ctk.CTkEntry(self.rightFrame,placeholder_text="año de estreno", width=120,height=50, font=("Times", 17,))
+        self.yearEntry=ctk.CTkEntry(self.centerFrame,placeholder_text="año de estreno", width=120,height=50, font=("Times", 17,))
         self.yearEntry.place(y=160,x=80)
 
+        #cuadro de derecha
+        self.rigthFrame=ctk.CTkFrame(self, width=390,height=900,fg_color="#fbf2e1")
+        self.rigthFrame.place(y=80,x=1000)
+        self.title2Label = ctk.CTkLabel(self.rigthFrame, text="Datos", font=("Times", 50, "bold")).place(y=30,relx=0.5, anchor="center")
 
-        #Cuadrado inferior derecho para dar
-        self.frameDownRigth=ctk.CTkFrame(self.rightFrame,width=485, height=180, fg_color="#f7e5c4")
+        #Datos del nodo 
+        #Titulo
+        self.movieTitleLbl=ctk.CTkLabel(self.rigthFrame,text="Titulo", font=("Times", 20, "bold"))
+        self.movieTitleLbl.place(y=100,x=10)
+
+        #Año
+        self.movieYearLbl=ctk.CTkLabel(self.rigthFrame, text="Año", font=("Times", 20, "bold"))
+        self.movieYearLbl.place(y=140, x=10)
+        
+        #Ingresos a nivel mundial
+        self.movieWorldwideEarningsLbl= ctk.CTkLabel(self.rigthFrame, text="Ingresos mundiales",font=("Times", 20, "bold"))
+        self.movieWorldwideEarningsLbl.place(y=180,x=10)
+
+        #Ingresos a nivel nacional 
+        self.movieDomesticEarningsLbl=ctk.CTkLabel(self.rigthFrame, text="Ingresos nacionales",font=("Times", 20, "bold"))
+        self.movieDomesticEarningsLbl.place(y=220,x=10)
+
+        #Ingresos a nivel internacional
+        self.movieForeignEarningsLbl=ctk.CTkLabel(self.rigthFrame, text="Ingresos internacionales",font=("Times", 20, "bold"))
+        self.movieForeignEarningsLbl.place(y=260,x=10)
+
+        #Porcentaje deganancias domestico
+        self.movieDomesticPercentEarnings=ctk.CTkLabel(self.rigthFrame, text='% de ingresos nacionales',font=("Times", 20, "bold"))
+        self.movieDomesticPercentEarnings.place(y=300,x=10)
+
+        #porcentaje a nivel internacional
+        self.movieForeignPercentEarnings=ctk.CTkLabel(self.rigthFrame, text="% de Ingresos internacionales",font=("Times", 20, "bold"))
+        self.movieForeignPercentEarnings.place(y=340,x=10)
+
+        #Cuadrado inferior derecho para dar datos
+        self.frameDownRigth=ctk.CTkFrame(self.rigthFrame,width=390, height=180, fg_color="#f7e5c4")
         self.frameDownRigth.place(y=720,x=0)
         #Datos del cuadrado
+        #nivel del nodo
         self.nodeLevelLbl=ctk.CTkLabel(self.frameDownRigth, text="Nivel del nodo", font=("Times", 20, "bold"))
-        self.nodeLevelLbl.place(y=2)
+        self.nodeLevelLbl.place(y=2,x=0)
+        #blance del nodo
         self.balanceLbl=ctk.CTkLabel(self.frameDownRigth, text="Factor de balanceo", font=("Times", 20, "bold"))
-        self.balanceLbl.place(y=32)
+        self.balanceLbl.place(y=32,x=0)
+        #padre del nodo
         self.fatherLbl=ctk.CTkLabel(self.frameDownRigth, text="Padre del nodo", font=("Times", 20, "bold"))
-        self.fatherLbl.place(y=62)
+        self.fatherLbl.place(y=62,x=0)
+        #abuelo del nodo
         self.gfatherLbl=ctk.CTkLabel(self.frameDownRigth, text="Abuelo del nodo", font=("Times", 20, "bold"))
-        self.gfatherLbl.place(y=92)
+        self.gfatherLbl.place(y=92,x=0)
+        #tio del nodo
         self.uncleLbl=ctk.CTkLabel(self.frameDownRigth, text="Tio del nodo", font=("Times", 20, "bold"))
-        self.uncleLbl.place(y=122)
+        self.uncleLbl.place(y=122,x=10)
 
-        #cuadro de verdad a la derecha
-        self.rigth=ctk.CTkFrame(self, width=390,height=900,fg_color="#fbf2e1")
-        self.rigth.place(y=80,x=1000)
-        self.title2Label = ctk.CTkLabel(self.rigth, text="Datos", font=("Times", 50, "bold")).place(y=30,relx=0.5, anchor="center")
 
+    def calc
 
 
 
